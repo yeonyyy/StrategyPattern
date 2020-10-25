ec4b81f15dcbf7c6539ccc13128a984208af9131
yeonyyy <layoun9108@gmail.com>
2020-10-25 16:34:05 +0900

    commit test

--- StrategyPattern/ViewController.swift
+++ StrategyPattern/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
     }
     
     @IBAction func textMod(_ sender: Any) {
